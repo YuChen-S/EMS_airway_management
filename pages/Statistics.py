7d6d4d4baff2bf399e716e1e71a1cca9d0177181
Yu-Chen Shen <65219219+YuChen-S@users.noreply.github.com>
2023-03-16 22:06:18 +0800

    Add title

--- pages/Statistics.py
+++ pages/Statistics.py
@@ -11,10 +11,11 @@ st.sidebar.info(
 )
 
 
-
+st.markdown("### --- The descriptive statistics ---")
 descriptive_stat = Image.open(os.path.dirname(__file__)+'/statistic_figures/descriptive_stat.jpg')
 st.image(descriptive_stat, caption='')
 
+st.markdown("### --- The paired t test of top 5 airway managements ---")
 paired_t_test = Image.open(os.path.dirname(__file__)+'/statistic_figures/paried_t_test_top5.jpg')
 st.image(paired_t_test, caption='')
 
